4450cd9f991b20a2c4699e37e71523136fba12c1
Bhargav Desai <bhargav.vijay.desai@accenture.com>
2012-07-03 12:15:08 +0530

    Initial commit to Lib_app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LibDemo::Application.config.secret_token = 'fd0a3bcf5baf35651ea09d6040650e37693f0f102e071c773b2ee1b15a4f870a11881de67bea7dea15cebcf491422c1af692c58dc45b99633f5572e265c99517'
